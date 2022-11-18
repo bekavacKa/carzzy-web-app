a3c48cb1baa3f428ad7df07566a145577da3fa6f
bekavacKa <th.beka17@gmail.com>
2022-11-18 23:59:10 +0100

    responsiv meni

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Link, NavLink} from "react-router-dom";
-import { FaCar, FaConnectdevelop, FaUser } from "react-icons/fa";
+import { FaCar, FaConnectdevelop, FaEquals, FaUser } from "react-icons/fa";
 // import { MdSearch } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 import { useSelector } from "react-redux";
 
 import routeConfig from '../../config/routeConfig';
@@ -15,6 +17,8 @@ function Navigation() {
 	const adminMeni = ['dashboard', 'account', 'logout'];
 	const userMeni = [ 'account', 'logout'];
 
+	const [hambMeni, setHambMeni] = useState(false);
+
 	useEffect(()=> {
 		// console.log("From store in nav => ", currentUser);
 	},[currentUser]);
@@ -22,7 +26,7 @@ function Navigation() {
 	const userBtnLayout = () => {
 		return (
 		currentUser.hasOwnProperty('username') ?
-		<div className='nav-user-profile-info' >
+		<div className='main-nav-user-profile-info' >
 
 			<FaUser className='user-icon' />
 			<p> {currentUser.username} </p> 
@@ -34,67 +38,117 @@ function Navigation() {
 			}
 
 		</div> :
-		<div className='nav-user-profile-signin'>
+		<div className='main-nav-user-profile-signin'>
 			<NavLink to={routeConfig.SIGN_IN.url}>
 				{routeConfig.SIGN_IN.name}
 			</NavLink>
 		</div>
 		)
+	};
+
+	const handleHambClick = () => {
+		setHambMeni(!hambMeni);
+	}
+
+	const handleLinkClick = () => {
+		setHambMeni(false);
 	}
 
   return (
-    <div className='nav-wrapper'>
-		
-        <nav>
-			<div className='nav-links'>
-				<Link className='logo' to={routeConfig.HOME.url}>
-					<FaConnectdevelop />
-				</Link>
+	<>
+		<div className='nav-wrapper'>
+			
+			<nav className='main-nav'>
+				<div className='main-nav-links'>
+					<Link className='logo' to={routeConfig.HOME.url}>
+						<FaConnectdevelop />
+					</Link>
 
-				<NavLink  to={routeConfig.HOME.url}>
-					{routeConfig.HOME.name}
-				</NavLink>
+					<NavLink  to={routeConfig.HOME.url}>
+						{routeConfig.HOME.name}
+					</NavLink>
 
-				<NavLink to={routeConfig.SHOP.url}>
-					{routeConfig.SHOP.name}
-				</NavLink>
+					<NavLink to={routeConfig.SHOP.url}>
+						{routeConfig.SHOP.name}
+					</NavLink>
 
-				<NavLink to={routeConfig.ABOUT_US.url}>
-					{routeConfig.ABOUT_US.name}
-				</NavLink>
+					<NavLink to={routeConfig.ABOUT_US.url}>
+						{routeConfig.ABOUT_US.name}
+					</NavLink>
 
-				{/* <NavLink to={routeConfig.CONTACT.url}>
-					{routeConfig.CONTACT.name}
-				</NavLink> */}
+					{/* <NavLink to={routeConfig.CONTACT.url}>
+						{routeConfig.CONTACT.name}
+					</NavLink> */}
 
-
-			</div>
-
-			<div className='nav-user' >
-				<div className='nav-user-cart'>
-					{/* {
-						currentUser.hasOwnProperty('wallet') ?
-						<p className={`nav-user-wallet positive` }> {currentUser.wallet.toFixed(2)} <span>$</span> </p> :
-						<p>0.00 &nbsp; $ </p>
-					} */}
-					<ShopCart />
-					{/* <MdAddShoppingCart className='cart'/> */}
-				</div>
-				{/* <div className='nav-user-search'>
-					<input type="search" placeholder='Search'/>
-					<MdSearch/>
-				</div> */}
-				<div className='nav-user-profile' >
-					
-					{userBtnLayout()}
 
 				</div>
+
+				<div className='main-nav-user' >
+					<div className='main-nav-user-cart'>
+						{/* {
+							currentUser.hasOwnProperty('wallet') ?
+							<p className={`main-nav-user-wallet positive` }> {currentUser.wallet.toFixed(2)} <span>$</span> </p> :
+							<p>0.00 &nbsp; $ </p>
+						} */}
+						<ShopCart />
+						{/* <MdAddShoppingCart className='cart'/> */}
+					</div>
+					{/* <div className='main-nav-user-search'>
+						<input type="search" placeholder='Search'/>
+						<MdSearch/>
+					</div> */}
+					<div className='main-nav-user-profile' >
+						
+						{userBtnLayout()}
+
+					</div>
+				</div>
+			</nav>
+
+			<nav className='responsive-nav'>
+
+				<div className='responsive-nav-title'>
+					<Link className='logo' to={routeConfig.HOME.url}>
+						<FaConnectdevelop />
+					</Link>
+					<h3 className='title'>
+						CARZZY
+					</h3>
+				</div>
+
+				<div className='responsive-nav-btn' onClick={handleHambClick}>
+					{
+						!hambMeni ? 
+						<GiHamburgerMenu className='btn-hamb' />
+						:
+						<IoClose /> 
+					}
+				</div>
+
+			</nav>
+
+
+		</div>
+
+		{
+			hambMeni && 
+			<div className='responsive-meni'>
+					<NavLink className='responsive-meni' onClick={handleLinkClick} to={routeConfig.HOME.url}>
+						{routeConfig.HOME.name}
+					</NavLink>
+
+					<NavLink className='responsive-meni' onClick={handleLinkClick} to={routeConfig.SHOP.url}>
+						{routeConfig.SHOP.name}
+					</NavLink>
+
+					<NavLink className='responsive-meni' onClick={handleLinkClick} to={routeConfig.ABOUT_US.url}>
+						{routeConfig.ABOUT_US.name}
+					</NavLink>
 			</div>
+		}
 
-            
-        </nav>
-
-    </div>
+	
+	</>
   )
 }
 
