3e72bba0cac86668f34cfc693b3fc495e22aca8d
bekavacKa <th.beka17@gmail.com>
2022-12-08 19:04:25 +0100

    railway backend testing

--- frontend/src/config/backendUrlConfig.js
+++ frontend/src/config/backendUrlConfig.js
@@ -2,11 +2,16 @@
 export const BACKEND_URL_CONFIG = {
     BACKEND_URL: {
         // url: `http://localhost:4000`
-        // url: `https://carzzy-web-app-production.up.railway.app/`
-        url: `https://carzzy-backend-bekavac-ka.onrender.com`
+        // ? backend render
+        // url: `https://carzzy-backend-bekavac-ka.onrender.com`
+        // ? backend railway
+        url: `https://carzzy-web-app-production.up.railway.app/`
     },
     PRODUCT_IMAGE_PATH: {
         // url: `http://localhost:4000/images/productImages/`
-        url: `https://carzzy-backend-bekavac-ka.onrender.com/images/productImages/`
+        // ? backend render path
+        // url: `https://carzzy-backend-bekavac-ka.onrender.com/images/productImages/`
+        // ? backend railway path
+        url: `https://carzzy-web-app-production.up.railway.app/images/productImages/`
     }
 }
