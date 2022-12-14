fe007b697849e502fa96f1091bba7f9ac03bd09f
bekavacKa <th.beka17@gmail.com>
2022-12-14 23:49:44 +0100

    displaying all data statistics from db in admin dashboard

--- frontend/src/adminComponents/Stats/Stats.jsx
+++ frontend/src/adminComponents/Stats/Stats.jsx
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { setLoader } from '../../redux/loaderSlice';
 import AdminService from '../../services/AdminService';
 import AuthService from '../../services/AuthService';
+import BlogService from '../../services/BlogService';
+import ShopService from '../../services/ShopService';
 import './stats.scss';
 import StatsBox from './StatsBox';
 
@@ -12,13 +14,23 @@ function Stats() {
   const [products, setProducts] = useState(0);
   const [users, setUsers] = useState(0);
   const [subs, setSubs] = useState(0);
+  const [blogs, setBlogs] = useState(0);
+  const [impressions, setImpressions] = useState(0);
+  const [sponsors, setSponsors] = useState(0);
+  const [productCategories, setProductCategories] = useState(0);
+  const [banners, setBanners] = useState(0);
 
-  // TODO sve u jedan api call tribam napravit
+  // TODO refactoring required (make one call to the backend that will fetch all the statistics from once)
 
   useEffect(() => {
     getProductsFromService();
     getUsersFromService();
     getSubscriptionsFromService();
+    getAllBlogs();
+    getAllImpressions();
+    getAllSponsors();
+    getAllProductCategories();
+    getAllBanners();
   }, []);
 
   const getProductsFromService = () => {
@@ -59,12 +71,76 @@ function Stats() {
                 .finally(() => dispatch(setLoader(false)));
   }
 
+  const getAllBlogs = () => {
+    dispatch(setLoader(true))
+    BlogService.getBlogs()
+                .then((res) => {
+                  setBlogs(res.data.length);
+                })
+                .catch((err) => {
+                  console.log(err);
+                })
+                .finally(() => dispatch(setLoader(false)));
+  }
+
+  const getAllImpressions = () => {
+    dispatch(setLoader(true))
+    AuthService.getImpressions()
+                .then((res) => {
+                  setImpressions(res.data.length);
+                })
+                .catch((err) => {
+                  console.log(err);
+                })
+                .finally(() => dispatch(setLoader(false)));
+  }
+
+  const getAllSponsors = () => {
+    dispatch(setLoader(true))
+    ShopService.getSponsors()
+                .then((res) => {
+                  setSponsors(res.data.length);
+                })
+                .catch((err) => {
+                  console.log(err);
+                })
+                .finally(() => dispatch(setLoader(false)));
+  }
+
+  const getAllProductCategories = () => {
+    dispatch(setLoader(true))
+    ShopService.getCategories()
+                .then((res) => {
+                  setProductCategories(res.data.length);
+                })
+                .catch((err) => {
+                  console.log(err);
+                })
+                .finally(() => dispatch(setLoader(false)));
+  }
+  const getAllBanners = () => {
+    dispatch(setLoader(true))
+    ShopService.getBanners()
+                .then((res) => {
+                  setBanners(res.data.length);
+                })
+                .catch((err) => {
+                  console.log(err);
+                })
+                .finally(() => dispatch(setLoader(false)));
+  }
+
 
   return (
     <div className='stats-wrapper' >
       <StatsBox number={products} title="PRODUCTS" />
       <StatsBox number={users} title="USers"/>
       <StatsBox number={subs} title="Subscriptions" />
+      <StatsBox number={blogs} title="Blogs" />
+      <StatsBox number={impressions} title="impressions" />
+      <StatsBox number={sponsors} title="sponsors" />
+      <StatsBox number={productCategories} title="product Categories" />
+      <StatsBox number={banners} title="banners" />
     </div>
   )
 }
