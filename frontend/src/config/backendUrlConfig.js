f7e2f66d474c30aff3bdd59326c5adcce973f86d
bekavacKa <th.beka17@gmail.com>
2022-12-27 23:46:22 +0100

    backend change - onrender

--- frontend/src/config/backendUrlConfig.js
+++ frontend/src/config/backendUrlConfig.js
@@ -3,15 +3,15 @@ export const BACKEND_URL_CONFIG = {
     BACKEND_URL: {
         // url: `http://localhost:4000`
         // ? backend render
-        // url: `https://carzzy-backend-bekavac-ka.onrender.com`
+        url: `https://carzzy-backend-bekavac-ka.onrender.com`
         // ? backend railway
-        url: `https://carzzy-web-app-production.up.railway.app/`
+        // url: `https://carzzy-web-app-production.up.railway.app/`
     },
     PRODUCT_IMAGE_PATH: {
         // url: `http://localhost:4000/images/productImages/`
         // ? backend render path
-        // url: `https://carzzy-backend-bekavac-ka.onrender.com/images/productImages/`
+        url: `https://carzzy-backend-bekavac-ka.onrender.com/images/productImages/`
         // ? backend railway path
-        url: `https://carzzy-web-app-production.up.railway.app/images/productImages/`
+        // url: `https://carzzy-web-app-production.up.railway.app/images/productImages/`
     }
 }
